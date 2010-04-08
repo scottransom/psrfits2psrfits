8630110544ca5cf6aaedeecca83c5147bb302ba1
Scott Ransom <sransom@nrao.edu>
2010-04-08 17:23:24 -0400

    Output only one file and throw away partial rows.

--- write_psrfits.c
+++ write_psrfits.c
@@ -1,5 +1,5 @@
 /* write_psrfits.c */
-#define _ISOC99_SOURCE  // For long double strtold
+#define _ISOC99_SOURCE          // For long double strtold
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,35 +9,41 @@
 #define DEBUGOUT 0
 
 // Define different obs modes
-static const int search=SEARCH_MODE, fold=FOLD_MODE;
-int psrfits_obs_mode(const char *obs_mode) {
-    if (strncmp("SEARCH", obs_mode, 6)==0) { return(search); }
-    else if (strncmp("FOLD", obs_mode, 4)==0) { return(fold); }
-    else if (strncmp("PSR", obs_mode, 3)==0) { return(fold); }
-    else if (strncmp("CAL", obs_mode, 3)==0) { return(fold); }
-    else {
+static const int search = SEARCH_MODE, fold = FOLD_MODE;
+int psrfits_obs_mode(const char *obs_mode)
+{
+    if (strncmp("SEARCH", obs_mode, 6) == 0) {
+        return (search);
+    } else if (strncmp("FOLD", obs_mode, 4) == 0) {
+        return (fold);
+    } else if (strncmp("PSR", obs_mode, 3) == 0) {
+        return (fold);
+    } else if (strncmp("CAL", obs_mode, 3) == 0) {
+        return (fold);
+    } else {
         // TODO: what to do here? default to search for now
         printf("Warning: obs_mode '%s' not recognized, defaulting to SEARCH.\n",
-                obs_mode);
-        return(search);
+               obs_mode);
+        return (search);
     }
-    return(search);
+    return (search);
 }
 
-int psrfits_create(struct psrfits *pf) {
+int psrfits_create(struct psrfits *pf)
+{
     int itmp, *status;
     long double ldtmp;
     double dtmp;
     char ctmp[40];
     struct hdrinfo *hdr;
 
-    hdr = &(pf->hdr);        // dereference the ptr to the header struct
-    status = &(pf->status);  // dereference the ptr to the CFITSIO status
+    hdr = &(pf->hdr);           // dereference the ptr to the header struct
+    status = &(pf->status);     // dereference the ptr to the CFITSIO status
 
     // Figure out what mode this is 
-    int mode=0;
+    int mode = 0;
     mode = psrfits_obs_mode(hdr->obs_mode);
-    if (mode==fold) {
+    if (mode == fold) {
         if (hdr->onlyI)
             printf("Warning!  In folding mode and ONLY_I is set!\n");
         if (hdr->ds_time_fact > 1)
@@ -45,9 +51,8 @@ int psrfits_create(struct psrfits *pf) {
         if (hdr->ds_freq_fact > 1)
             printf("Warning!  In folding mode and DS_FREQ is > 1!\n");
     }
-
     // Initialize the key variables if needed
-    if (pf->filenum == 0) {  // first time writing to the file
+    if (pf->filenum == 0) {     // first time writing to the file
         pf->status = 0;
         pf->tot_rows = 0;
         pf->N = 0L;
@@ -59,7 +64,7 @@ int psrfits_create(struct psrfits *pf) {
         char datadir[1024];
         strncpy(datadir, pf->basefilename, 1023);
         char *last_slash = strrchr(datadir, '/');
-        if (last_slash!=NULL && last_slash!=datadir) {
+        if (last_slash != NULL && last_slash != datadir) {
             *last_slash = '\0';
             printf("Using directory '%s' for output.\n", datadir);
             char cmd[1024];
@@ -73,7 +78,7 @@ int psrfits_create(struct psrfits *pf) {
 
     // Update the filename - don't include filenum for fold mode
     // TODO : use rf/cf extensions for psr/cals?
-    if (mode==fold && pf->multifile!=1)
+    if (mode == fold && pf->multifile != 1)
         sprintf(pf->filename, "%s.fits", pf->basefilename);
     else
         sprintf(pf->filename, "%s_%04d.fits", pf->basefilename, pf->filenum);
@@ -81,7 +86,7 @@ int psrfits_create(struct psrfits *pf) {
     // Create basic FITS file from our template
     char template_file[1024];
     printf("Opening file '%s' ", pf->filename);
-    if (mode==search) { 
+    if (mode == search) {
         printf("in search mode.\n");
         sprintf(template_file, "%s/%s", SRCDIR, PSRFITS_SEARCH_TEMPLATE);
     }
@@ -93,7 +98,6 @@ int psrfits_create(struct psrfits *pf) {
         fits_report_error(stderr, *status);
         exit(1);
     }
-
     // Go to the primary HDU
     fits_movabs_hdu(pf->fptr, 1, NULL, status);
 
@@ -101,20 +105,20 @@ int psrfits_create(struct psrfits *pf) {
     fits_get_system_time(ctmp, &itmp, status);
     // Note:  this is the date the file was _written_, not the obs start date
     fits_update_key(pf->fptr, TSTRING, "DATE", ctmp, NULL, status);
-    fits_update_key(pf->fptr, TSTRING, "TELESCOP", hdr->telescope,NULL, status);
+    fits_update_key(pf->fptr, TSTRING, "TELESCOP", hdr->telescope, NULL, status);
     fits_update_key(pf->fptr, TSTRING, "OBSERVER", hdr->observer, NULL, status);
     fits_update_key(pf->fptr, TSTRING, "PROJID", hdr->project_id, NULL, status);
     fits_update_key(pf->fptr, TSTRING, "FRONTEND", hdr->frontend, NULL, status);
     fits_update_key(pf->fptr, TSTRING, "BACKEND", hdr->backend, NULL, status);
     if (hdr->onlyI || hdr->summed_polns) {
         if (!hdr->onlyI && hdr->npol > 1) {
-            printf("Warning!:  Can't have %d polarizations _and_ be summed!\n", 
+            printf("Warning!:  Can't have %d polarizations _and_ be summed!\n",
                    hdr->npol);
         }
         itmp = 2;
         fits_update_key(pf->fptr, TINT, "NRCVR", &itmp, NULL, status);
     } else {
-        if (hdr->npol > 2) { // Can't have more than 2 real polns (i.e. NRCVR)
+        if (hdr->npol > 2) {    // Can't have more than 2 real polns (i.e. NRCVR)
             itmp = 2;
             fits_update_key(pf->fptr, TINT, "NRCVR", &itmp, NULL, status);
         } else {
@@ -127,9 +131,8 @@ int psrfits_create(struct psrfits *pf) {
     fits_update_key(pf->fptr, TDOUBLE, "FD_XYPH", &(hdr->fd_xyph), NULL, status);
     fits_update_key(pf->fptr, TINT, "BE_PHASE", &(hdr->be_phase), NULL, status);
     fits_update_key(pf->fptr, TSTRING, "DATE-OBS", hdr->date_obs, NULL, status);
-    if (mode==fold && !strcmp("CAL",hdr->obs_mode)) 
-        fits_update_key(pf->fptr, TSTRING, "OBS_MODE", hdr->obs_mode, 
-                NULL, status);
+    if (mode == fold && !strcmp("CAL", hdr->obs_mode))
+        fits_update_key(pf->fptr, TSTRING, "OBS_MODE", hdr->obs_mode, NULL, status);
     fits_update_key(pf->fptr, TDOUBLE, "OBSFREQ", &(hdr->fctr), NULL, status);
     fits_update_key(pf->fptr, TDOUBLE, "OBSBW", &(hdr->BW), NULL, status);
     fits_update_key(pf->fptr, TINT, "OBSNCHAN", &(hdr->orig_nchan), NULL, status);
@@ -150,8 +153,10 @@ int psrfits_create(struct psrfits *pf) {
     fits_update_key(pf->fptr, TDOUBLE, "BMAJ", &(hdr->beam_FWHM), NULL, status);
     fits_update_key(pf->fptr, TDOUBLE, "BMIN", &(hdr->beam_FWHM), NULL, status);
     if (strcmp("OFF", hdr->cal_mode)) {
-        fits_update_key(pf->fptr, TDOUBLE, "CAL_FREQ", &(hdr->cal_freq), NULL, status);
-        fits_update_key(pf->fptr, TDOUBLE, "CAL_DCYC", &(hdr->cal_dcyc), NULL, status);
+        fits_update_key(pf->fptr, TDOUBLE, "CAL_FREQ", &(hdr->cal_freq), NULL,
+                        status);
+        fits_update_key(pf->fptr, TDOUBLE, "CAL_DCYC", &(hdr->cal_dcyc), NULL,
+                        status);
         fits_update_key(pf->fptr, TDOUBLE, "CAL_PHS", &(hdr->cal_phs), NULL, status);
     }
     fits_update_key(pf->fptr, TDOUBLE, "SCANLEN", &(hdr->scanlen), NULL, status);
@@ -177,11 +182,11 @@ int psrfits_create(struct psrfits *pf) {
     }
     if (!hdr->onlyI && !hdr->summed_polns) {
         // TODO:  These need to be updated for the real machine.
-        if (hdr->npol==1)
+        if (hdr->npol == 1)
             strcpy(ctmp, "AA");
-        else if (hdr->npol==2)
+        else if (hdr->npol == 2)
             strcpy(ctmp, "AABB");
-        else if (hdr->npol==4)
+        else if (hdr->npol == 4)
             strcpy(ctmp, "IQUV");
         fits_update_key(pf->fptr, TSTRING, "POL_TYPE", ctmp, NULL, status);
     } else {
@@ -195,69 +200,70 @@ int psrfits_create(struct psrfits *pf) {
     fits_update_key(pf->fptr, TINT, "NCHAN", &itmp, NULL, status);
     dtmp = hdr->df * hdr->ds_freq_fact;
     fits_update_key(pf->fptr, TDOUBLE, "CHAN_BW", &dtmp, NULL, status);
-    if (mode==search) {
+    if (mode == search) {
         int out_nsblk = hdr->nsblk / hdr->ds_time_fact;
         itmp = 1;
         fits_update_key(pf->fptr, TINT, "NSBLK", &out_nsblk, NULL, status);
         fits_update_key(pf->fptr, TINT, "NBITS", &(hdr->nbits), NULL, status);
         fits_update_key(pf->fptr, TINT, "NBIN", &itmp, NULL, status);
-    } else if (mode==fold) {
+    } else if (mode == fold) {
         itmp = 1;
         fits_update_key(pf->fptr, TINT, "NSBLK", &itmp, NULL, status);
         fits_update_key(pf->fptr, TINT, "NBITS", &itmp, NULL, status);
         fits_update_key(pf->fptr, TINT, "NBIN", &(hdr->nbin), NULL, status);
         fits_update_key(pf->fptr, TSTRING, "EPOCHS", "MIDTIME", NULL, status);
     }
-
     // Update the column sizes for the colums containing arrays
     {
         int out_npol = hdr->npol;
         int out_nchan = hdr->nchan / hdr->ds_freq_fact;
-        if (hdr->onlyI) out_npol = 1;
+        if (hdr->onlyI)
+            out_npol = 1;
         int out_nsblk = hdr->nsblk / hdr->ds_time_fact;
 
-        fits_modify_vector_len(pf->fptr, 13, out_nchan, status); // DAT_FREQ
-        fits_modify_vector_len(pf->fptr, 14, out_nchan, status); // DAT_WTS
+        fits_modify_vector_len(pf->fptr, 13, out_nchan, status);        // DAT_FREQ
+        fits_modify_vector_len(pf->fptr, 14, out_nchan, status);        // DAT_WTS
         itmp = out_nchan * out_npol;
-        fits_modify_vector_len(pf->fptr, 15, itmp, status); // DAT_OFFS
-        fits_modify_vector_len(pf->fptr, 16, itmp, status); // DAT_SCL
-        
-        if (mode==search)
+        fits_modify_vector_len(pf->fptr, 15, itmp, status);     // DAT_OFFS
+        fits_modify_vector_len(pf->fptr, 16, itmp, status);     // DAT_SCL
+
+        if (mode == search)
             itmp = (hdr->nbits * out_nchan * out_npol * out_nsblk) / 8;
-        else if (mode==fold)
+        else if (mode == fold)
             itmp = (hdr->nbin * out_nchan * out_npol);
-        fits_modify_vector_len(pf->fptr, 17, itmp, status); // DATA
+        fits_modify_vector_len(pf->fptr, 17, itmp, status);     // DATA
         // Update the TDIM field for the data column
-        if (mode==search)
+        if (mode == search)
             sprintf(ctmp, "(1,%d,%d,%d)", out_nchan, out_npol, out_nsblk);
-        else if (mode==fold) 
+        else if (mode == fold)
             sprintf(ctmp, "(%d,%d,%d,1)", hdr->nbin, out_nchan, out_npol);
         fits_update_key(pf->fptr, TSTRING, "TDIM17", ctmp, NULL, status);
     }
 
     fits_flush_file(pf->fptr, status);
-    
+
     return *status;
 }
 
 
-int psrfits_write_subint(struct psrfits *pf) {
-  int row, *status, nchan, nivals, mode, out_nbytes, nstat, dummy;
-  float ftmp;
-  struct hdrinfo *hdr;
-  struct subint *sub;
+int psrfits_write_subint(struct psrfits *pf)
+{
+    int row, *status, nchan, nivals, mode, out_nbytes, nstat, dummy;
+    float ftmp;
+    struct hdrinfo *hdr;
+    struct subint *sub;
 
-    hdr = &(pf->hdr);        // dereference the ptr to the header struct
-    sub = &(pf->sub);        // dereference the ptr to the subint struct
-    status = &(pf->status);  // dereference the ptr to the CFITSIO status
+    hdr = &(pf->hdr);           // dereference the ptr to the header struct
+    sub = &(pf->sub);           // dereference the ptr to the subint struct
+    status = &(pf->status);     // dereference the ptr to the CFITSIO status
     nchan = hdr->nchan / hdr->ds_freq_fact;
-    nstat = sub->statbytes_per_subint / 2; //stat array is shorts
+    nstat = sub->statbytes_per_subint / 2;      //stat array is shorts
     if (hdr->onlyI)
         nivals = nchan;
     else
         nivals = nchan * hdr->npol;
     mode = psrfits_obs_mode(hdr->obs_mode);
-    if (mode==fold)
+    if (mode == fold)
         out_nbytes = sub->bytes_per_subint / hdr->ds_freq_fact;
     else {
         out_nbytes = sub->bytes_per_subint / (hdr->ds_freq_fact * hdr->ds_time_fact);
@@ -267,28 +273,30 @@ int psrfits_write_subint(struct psrfits *pf) {
 
     // Create the initial file or change to a new one if needed.
     // Stay with a single file for fold mode.
-    
+
     //fprintf(stderr,"In psrfits write, pf->filenum: %d pf->multifile: %d\n",pf->filenum,pf->multifile);
     //fprintf(stderr,"In psrfits write, pf->rownum %d pf->rows_per_file: %d\n",pf->rownum,pf->rows_per_file);
 
     /*
-    if (pf->filenum==0 || 
-            ( (mode==search || pf->multifile==1) 
-              && pf->rownum > pf->rows_per_file)) {
-        if (pf->filenum) {
-            printf("Closing file '%s'\n", pf->filename);
-            fits_close_file(pf->fptr, status);
-	    return *status;
-        }
-        psrfits_create(pf);
-    }
-    */
+       if (pf->filenum==0 || 
+       ( (mode==search || pf->multifile==1) 
+       && pf->rownum > pf->rows_per_file)) {
+       if (pf->filenum) {
+       printf("Closing file '%s'\n", pf->filename);
+       fits_close_file(pf->fptr, status);
+       return *status;
+       }
+       psrfits_create(pf);
+       }
+     */
 
     row = pf->rownum;
     //fprintf(stderr,"In psrfits write, row: %d\n",row);
 
     //fits_read_key(pf->fptr, TINT, "NAXIS2", &dummy, NULL, status);
     //fprintf(stderr,"In psrfits write, before col writing, NAXIS2: %d status; %d\n",dummy,*status);
+
+    fprintf(stderr, "tsubint: %f  offs_sub: %f\n", sub->tsubint, sub->offs);
 
     fits_write_col(pf->fptr, TDOUBLE, 1, row, 1, 1, &(sub->tsubint), status);
     fits_write_col(pf->fptr, TDOUBLE, 2, row, 1, 1, &(sub->offs), status);
@@ -312,23 +320,22 @@ int psrfits_write_subint(struct psrfits *pf) {
     fits_write_col(pf->fptr, TFLOAT, 15, row, 1, nivals, sub->dat_offsets, status);
     fits_write_col(pf->fptr, TFLOAT, 16, row, 1, nivals, sub->dat_scales, status);
 
-    if (mode==search) {
-      // Need to change this for other data types...
-      //fprintf(stderr,"In psrfits write, out_nbytes (data bytes per subint): %d\n",out_nbytes);
-      fits_write_col(pf->fptr, TBYTE, 17, row, 1, out_nbytes, 
-		     sub->data, status);
-    } else if (mode==fold) { 
-      // Fold mode writes floats for now..
-      fits_write_col(pf->fptr, TFLOAT, 17, row, 1, out_nbytes/sizeof(float), 
-		     sub->data, status);
+    if (mode == search) {
+        // Need to change this for other data types...
+        //fprintf(stderr,"In psrfits write, out_nbytes (data bytes per subint): %d\n",out_nbytes);
+        fits_write_col(pf->fptr, TBYTE, 17, row, 1, out_nbytes, sub->data, status);
+    } else if (mode == fold) {
+        // Fold mode writes floats for now..
+        fits_write_col(pf->fptr, TFLOAT, 17, row, 1, out_nbytes / sizeof(float),
+                       sub->data, status);
     }
 
     /*
-    fprintf(stderr,"In psrfits write, out_nbytes: %d, nchan: %d nivals: %d nstat: %d status: %d\n",out_nbytes,nchan,nivals,nstat,*status);
-    fits_flush_file(pf->fptr, status);
-    fits_read_key(pf->fptr, TINT, "NAXIS2", &dummy, NULL, status);
-    fprintf(stderr,"In psrfits write after 17 column, NAXIS2: %d status; %d\n",dummy,*status);
-    */
+       fprintf(stderr,"In psrfits write, out_nbytes: %d, nchan: %d nivals: %d nstat: %d status: %d\n",out_nbytes,nchan,nivals,nstat,*status);
+       fits_flush_file(pf->fptr, status);
+       fits_read_key(pf->fptr, TINT, "NAXIS2", &dummy, NULL, status);
+       fprintf(stderr,"In psrfits write after 17 column, NAXIS2: %d status; %d\n",dummy,*status);
+     */
 
     fits_write_col(pf->fptr, TSHORT, 18, row, 1, nstat, sub->stat, status);
 
@@ -353,7 +360,6 @@ int psrfits_write_subint(struct psrfits *pf) {
         fits_report_error(stderr, *status);
         fflush(stderr);
     }
-
     // Now update some key values if no CFITSIO errors
     if (!(*status)) {
         pf->rownum++;
@@ -362,26 +368,26 @@ int psrfits_write_subint(struct psrfits *pf) {
         pf->T += sub->tsubint;
 
         // For fold mode, print info each subint written
-        if (mode==fold && pf->quiet!=1) {
-            printf("Wrote subint %d (total time %.1fs)\n", pf->rownum-1, pf->T);
+        if (mode == fold && pf->quiet != 1) {
+            printf("Wrote subint %d (total time %.1fs)\n", pf->rownum - 1, pf->T);
             fflush(stdout);
         }
 
     }
-
     //fits_update_key(pf->fptr,TINT,"NAXIS2",&(pf->tot_rows),NULL,status);
 
     return *status;
 }
 
 
-int psrfits_close(struct psrfits *pf) {
+int psrfits_close(struct psrfits *pf)
+{
     if (!pf->status) {
         fits_close_file(pf->fptr, &(pf->status));
         printf("Closing file '%s'\n", pf->filename);
     }
     printf("Done.  %s %d subints (%f sec) in %d files (status = %d).\n",
-            pf->mode=='r' ? "Read" : "Wrote", 
-            pf->tot_rows, pf->T, pf->filenum, pf->status);
+           pf->mode == 'r' ? "Read" : "Wrote",
+           pf->tot_rows, pf->T, pf->filenum, pf->status);
     return pf->status;
 }
